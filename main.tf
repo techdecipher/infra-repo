92c75c31bf98583db53c38c3a91aed614e6d41a2
techdecipher <waytopranav22@gmail.com>
2025-08-05 12:48:37 +0530

    updated main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+# EXISTING: VPC + Subnets + IAM + EKS + ECR
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
@@ -34,7 +35,6 @@ resource "aws_iam_role" "eks_cluster_role" {
   }
 }
 
-
 resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSClusterPolicy" {
   role       = aws_iam_role.eks_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
@@ -64,8 +64,26 @@ resource "aws_ecr_repository" "flask_app" {
   }
 }
 
+# 🔺 ADD THIS BLOCK
+data "aws_eks_cluster" "cluster" {
+  name = aws_eks_cluster.eks.name
+}
+
+# 🔺 ADD THIS BLOCK
+data "aws_eks_cluster_auth" "cluster" {
+  name = aws_eks_cluster.eks.name
+}
+
+# 🔺 ADD THIS PROVIDER BLOCK
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+# ✅ ALREADY CORRECT: aws-auth ConfigMap (just make sure it's at the end)
 resource "kubernetes_config_map" "aws_auth" {
-  depends_on = [aws_eks_cluster.eks]  # Ensure EKS is ready first
+  depends_on = [aws_eks_cluster.eks]
 
   metadata {
     name      = "aws-auth"
@@ -82,12 +100,10 @@ resource "kubernetes_config_map" "aws_auth" {
     ])
     mapUsers = yamlencode([
       {
-        userarn  = "arn:aws:iam::405325454731:user/pranav"  
+        userarn  = "arn:aws:iam::405325454731:user/pranav"
         username = "pranav"
         groups   = ["system:masters"]
       }
     ])
   }
 }
-
-
