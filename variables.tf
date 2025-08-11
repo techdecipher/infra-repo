fbab016a3653f43b24a55a0b052d4d0f7e65da36
Pranav Chaudhari <waytopranav22@gmail.com>
2025-08-11 15:11:44 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "region"        { default = "us-east-1" }
+variable "instance_type" { default = "t3.nano" }
+variable "vpc_cidr"      { default = "10.10.0.0/16" }
+variable "subnet_cidr"   { default = "10.10.1.0/24" }
