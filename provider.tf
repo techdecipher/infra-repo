e2d7929909f1b173ac758a8fda5c10378bdc7c6f
Pranav Chaudhari <waytopranav22@gmail.com>
2025-08-11 16:06:23 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,12 +1,16 @@
 terraform {
   required_version = ">= 1.6.0"
+
   required_providers {
-    aws = { source = "hashicorp/aws", version = "~> 5.50" }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.50"
+    }
   }
 
   backend "s3" {
-    bucket = "k8s-state99"               # your existing S3 bucket
-    key    = "tfstate/ec2-infra.tfstate" # state file path in bucket
+    bucket = "k8s-state99"                # your existing S3 bucket
+    key    = "tfstate/ec2-infra.tfstate"  # state file path in bucket
     region = "us-east-1"                  # bucket's region
   }
 }
