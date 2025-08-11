5e7c39af2e3715447d463a228380c34dea347c8f
Pranav Chaudhari <waytopranav22@gmail.com>
2025-08-11 15:17:58 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,23 @@
-variable "region"        { default = "us-east-1" }
-variable "instance_type" { default = "t3.nano" }
-variable "vpc_cidr"      { default = "10.10.0.0/16" }
-variable "subnet_cidr"   { default = "10.10.1.0/24" }
+variable "region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.nano"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.10.0.0/16"
+}
+
+variable "subnet_cidr" {
+  description = "CIDR block for public subnet"
+  type        = string
+  default     = "10.10.1.0/24"
+}
